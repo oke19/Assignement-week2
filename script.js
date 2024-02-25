46309a7f217fe238ce733579fa393583ed749b9c
unknown <ezekielpromzyokechukwu@gmail.com>
2024-02-24 21:02:28 -0800

    Add script.js file and update

---
+++ script.js
@@ -0,0 +1,22 @@
+let prompt = require("prompt-sync")()
+
+//get year of birth 
+//get current year
+//subtract year of birth from current year to get the present age of MR. john
+
+let yearofbirth = prompt("1950 ")
+
+
+
+
+let currentyear = prompt("2024")
+
+
+let difference = prompt("1950-2024")
+
+
+
+console.log("hello {he was born in-1950} &{current year is-2024} He's presently 74 years old")
+
+
+
